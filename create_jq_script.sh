608c15346a712a70d55b911a34d006ffddfdc8b9
Jan Bobrowski <janbobrowski@gmail.com>
2024-06-09 11:44:21 +0200

    create script scripts

---
+++ create_jq_script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+output_script_name=script.jq
+
+help()
+{
+  THIS_SCRIPT_NAME=$(basename "$0")
+  echo "Creates jq script with shebang line and 755 permissions" >&2
+  echo "Usage: ./$THIS_SCRIPT_NAME [output_script_name=$output_script_name]" >&2
+}
+
+if [ "${1:0:1}" == "-" ]; then
+    help
+    exit 0
+fi
+
+if (( "${#}" > 0 )); then
+  output_script_name="${1}"
+else
+  echo "Using default script name: $output_script_name"
+fi
+
+if [ -f "${output_script_name}" ]; then
+  echo "Script ${output_script_name}" already exists
+  exit 0
+fi
+
+jq -nr '"#!/usr/bin/jq -f"' | install --mod=755 /dev/stdin "${output_script_name}"
+
+echo "Created script ${output_script_name}"
