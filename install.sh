b2ebc8373e299db850747d23b9b7df8a4980f917
Jan Bobrowski <janbobrowski@gmail.com>
2024-10-27 12:13:53 +0100

    removed exit from install.sh

--- install.sh
+++ install.sh
@@ -7,26 +7,19 @@ echo "~/.bashrc file will be modified to add ${THIS_SCRIPT_DIR_PATH} to the PATH
 read answer
 
 if [ "$answer" != "y" ] ; then
-    exit
-fi
-
-if [ $(grep -ic "${THIS_SCRIPT_DIR}" ~/.bashrc) -gt 0 ]; then
+    :
+elif [ $(grep -ic "${THIS_SCRIPT_DIR}" ~/.bashrc) -gt 0 ]; then
     echo .bashrc already contains "${THIS_SCRIPT_DIR}"
     echo Please add following line at the end of .bashrc file:
     echo PATH=\$PATH:${THIS_SCRIPT_DIR_PATH}
-    exit
-fi
-
-if [ ! -w ~/.bashrc ]; then
+elif [ ! -w ~/.bashrc ]; then
     echo Cannot modify .bashrc
     echo Please add following line at the end of .bashrc file:
     echo PATH=\$PATH:${THIS_SCRIPT_DIR_PATH}
     echo or add ${THIS_SCRIPT_DIR_PATH} to your PATH in other way
-    exit
+else
+    echo PATH=\$PATH:${THIS_SCRIPT_DIR_PATH} | tee -a ~/.bashrc
+    echo ~/.bashrc file have been modified
+    echo running source ~/.bashrc
+    source ~/.bashrc
 fi
-
-echo PATH=\$PATH:${THIS_SCRIPT_DIR_PATH} | tee -a ~/.bashrc
-
-echo ~/.bashrc file have been modified
-echo running source ~/.bashrc
-source ~/.bashrc
