e4d0576695bc1a07398bbe89b7a97794a744ef93
Jan Bobrowski <janbobrowski@gmail.com>
2024-10-27 12:04:35 +0100

    improved install.sh

--- install.sh
+++ install.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
-if [ ${PWD##*/} != "jbscripts" ]; then
-    echo "Please run this script from jbscripts directory"
-    exit
-fi
+THIS_SCRIPT_DIR_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+THIS_SCRIPT_DIR=$( basename ${THIS_SCRIPT_DIR_PATH} )
 
-echo '~/.bashrc file will be modified to add jbscripts to the PATH, do you want to continue (y/n)?'
+echo "~/.bashrc file will be modified to add ${THIS_SCRIPT_DIR_PATH} to the PATH, do you want to continue (y/n)?"
 read answer
 
 if [ "$answer" != "y" ] ; then
     exit
 fi
 
-if [ $(grep -ic "jbscripts" ~/.bashrc) -gt 0 ]; then
-    echo .bashrc already contains "jbscripts"
-    echo Please add manually the following line at the end .bashrc file: PATH=\$PATH:${PWD}
+if [ $(grep -ic "${THIS_SCRIPT_DIR}" ~/.bashrc) -gt 0 ]; then
+    echo .bashrc already contains "${THIS_SCRIPT_DIR}"
+    echo Please add following line at the end of .bashrc file:
+    echo PATH=\$PATH:${THIS_SCRIPT_DIR_PATH}
     exit
 fi
 
 if [ ! -w ~/.bashrc ]; then
     echo Cannot modify .bashrc
-    echo Please add the following line at the end .bashrc file: PATH=\$PATH:${PWD}
-    echo or add jbscripts to your PATH in other way
+    echo Please add following line at the end of .bashrc file:
+    echo PATH=\$PATH:${THIS_SCRIPT_DIR_PATH}
+    echo or add ${THIS_SCRIPT_DIR_PATH} to your PATH in other way
     exit
 fi
 
-echo PATH=\$PATH:${PWD} | tee -a ~/.bashrc
+echo PATH=\$PATH:${THIS_SCRIPT_DIR_PATH} | tee -a ~/.bashrc
 
 echo ~/.bashrc file have been modified
+echo running source ~/.bashrc
+source ~/.bashrc
